2dbaa0024592cc8bb10416efd082a1d1df2b9e6e
Adam Byrne <adamrbyrne@gmail.com>
2023-06-01 10:42:22 +0100

    request integrations

--- UserService/src/main/java/com/johara/user/UserServiceApplication.java
+++ UserService/src/main/java/com/johara/user/UserServiceApplication.java
@@ -1,4 +1,4 @@
-package com.johara.order;
+package com.johara.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
--- UserService/src/main/resources/data.sql
+++ UserService/src/main/resources/data.sql
@@ -1,12 +1,11 @@
-DROP TABLE IF EXISTS user;
-
-CREATE TABLE user (
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL
 );
 
-INSERT INTO user (name, email)
+INSERT INTO users (name, email)
 VALUES
     ('John Doe', 'john@example.com'),
     ('Jane Smith', 'jane@example.com'),
